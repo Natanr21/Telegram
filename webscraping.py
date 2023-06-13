ee160f8e4531221fcd9328ad093b0175a66cc899
Natan Rodrigues <128554031+Natanr21@users.noreply.github.com>
2023-06-13 12:35:33 -0300

    Web Scraping

--- webscraping.py
+++ webscraping.py
@@ -1,27 +1,33 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
 
 import time
 
 # Buscar as informações
 nav = webdriver.Chrome()
+action_chains = ActionChains(nav)
 
-link = 'https://br.betano.com/'
+link = 'https://estrelabet.com/ptb/bet/main'
 
 nav.get(link)
 
-time.sleep(1)
+time.sleep(3)
 
-quit_ad = nav.find_elements_by_xpath('//*[@id="landing-page-modal"]/div/div[1]/button/svg').click()
+click_cookies = nav.find_element(By.XPATH,'//*[@id="cookies-bottom-modal"]/div/div[1]/a')
+action_chains.click(click_cookies).perform()
+time.sleep(5)
 
-#quit_ad.click()
+brasileirao23 = nav.find_element(By.XPATH,'//*[@id="container-main-right"]/league-card/div/div[1]/div/a')
+action_chains.click(brasileirao23).perform()
+time.sleep(10)
 
-time.sleep(15)
+
 
 #print()
-
-
+#
+#//*[@id="trader-estrelabet"]
 
 #<span class="sgl-ParticipantOddsOnly80_Odds">2.40</span>
 # Salvar em um dicionário
